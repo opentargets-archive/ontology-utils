e82574fc5737635a06d33f1393721543ee5198a4
Gautier Koscielny <gautier.x.koscielny@gsk.com>
2015-01-15 15:25:53 +0000

    check file path

--- src/python/ontologyutils/tests_ontologyutils.py
+++ src/python/ontologyutils/tests_ontologyutils.py
@@ -3,6 +3,23 @@ import pytest
 sys.path.append('./ontologyutils')
 import ontologyutils as onto
 
+print("Path at terminal when executing this file")
+print(os.getcwd() + "\n")
+
+print("This file path, relative to os.getcwd()")
+print(__file__ + "\n")
+
+print("This file full path (following symlinks)")
+full_path = os.path.realpath(__file__)
+print(full_path + "\n")
+
+print("This file directory and name")
+path, file = os.path.split(full_path)
+print(path + ' --> ' + file + "\n")
+
+print("This file directory only")
+print(os.path.dirname(full_path))
+
 filename = '../samples/hp.obo'
 hpo = onto.Ontology.fromOBOFile(filename)
 
