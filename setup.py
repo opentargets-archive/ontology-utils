deed81fc38c840d8f66bcb338260f7446f840a93
Gautier Koscielny <gkos.bio@gmail.com>
2018-02-16 18:52:16 +0000

    new version

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).r
 
 setup(
     name="ontologyutils",
-    version="0.1.1",
+    version="0.2",
     description=long_description.split("\n")[0],
     long_description=long_description,
     author="Gautier Koscielny",
     author_email="gautier.x.koscielny@gsk.com",
-    url="https://github.com/CTTV",
+    url="https://github.com/opentargets",
     packages=["ontologyutils"],
     license="Apache2",
     classifiers=[
