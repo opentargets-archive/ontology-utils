5b0530390251a1a7d80afb0b3afa8d9e72a617ff
Adam Faulconbridge <afaulconbridge@googlemail.com>
2019-03-11 11:36:11 +0000

    use list not set

--- opentargets_ontologyutils/efo.py
+++ opentargets_ontologyutils/efo.py
@@ -41,21 +41,23 @@ def load_open_targets_disease_ontology(ocr, efo_uri):
     logger.debug("Found %d therapeutic areas", len(therapeutic_areas))
 
     #combine a dictionary of which therapeutic areas each term is in
-    ocr.therapeutic_labels = collections.defaultdict(set)
-    ocr.therapeutic_uris = collections.defaultdict(set)
+    ocr.therapeutic_labels = collections.defaultdict(list)
+    ocr.therapeutic_uris = collections.defaultdict(list)
     for uri in ocr.classes_paths:
         #check if this uri is a therapeutic area itself
         if uri in therapeutic_areas:
             label = ocr.current_classes[uri]
-            ocr.therapeutic_labels[uri].add(label)
-            ocr.therapeutic_uris[uri].add(uri)
+            if label not in ocr.therapeutic_labels[uri]:
+                ocr.therapeutic_labels[uri].append(label)
+                ocr.therapeutic_uris[uri].append(uri)
         #follow all paths to root and check if therapeutic area
         for path in ocr.classes_paths[uri]['all']:
             for entry in path:
                 if entry['uri'] in therapeutic_areas:
                     label = ocr.current_classes[entry['uri']]
-                    ocr.therapeutic_labels[uri].add(label)
-                    ocr.therapeutic_uris[uri].add(entry['uri'])
+                    if label not in ocr.therapeutic_labels[uri]:
+                        ocr.therapeutic_labels[uri].append(label)
+                        ocr.therapeutic_uris[uri].append(entry['uri'])
 
 
 """
