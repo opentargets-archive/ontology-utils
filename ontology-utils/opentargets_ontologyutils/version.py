0ea893f64e695845a9bf64ce822a8f3da4a005a1
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2020-03-11 12:23:02 +0000

    Upgate package version to 1.2.4

---
+++ ontology-utils/opentargets_ontologyutils/version.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+__pkgname__ = 'opentargets-ontologyutils'
+__author__ = 'OpenTargets Core Team <support@targetvalidation.org>'
+__author_email__ = 'support@targetvalidation.org'
+__license__ = 'Apache License, Version 2.0'
+__homepage__ = 'https://github.com/opentargets/ontology-utils'
+__version__ = '1.2.4'
+__description__ = 'Ontology utilities at targetvalidation.org'
